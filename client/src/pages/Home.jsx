95dd1689fefdfa13282290451b7bf0ee6ff2974e
nampm9x <nampew2004@gmail.com>
2024-11-07 21:55:17 +0700

    install server project

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <div>
+        <img className='w-full' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDdRxuf1B4j7CeKyEP5BLk-AGAx_CjBZmW8w&s" alt="" />
+      </div>
+    </div>
   )
 }
