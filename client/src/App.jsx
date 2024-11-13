804376bb8ce57d566a92517aba25f098e9d28408
nampm9x <nampew2004@gmail.com>
2024-11-13 21:10:38 +0700

    add firebase to my project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,41 @@
-import{BrowserRouter, Route, Routes} from 'react-router-dom'
-import Header from "./components/Header"
-import Home from './pages/Home'
-import Product from'./pages/Product'
-import Register from './pages/Register'
-import Login from './pages/Login' 
-import Test from './pages/Test'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Test from "./pages/Test";
+import { useSelector } from "react-redux";
+import AdminHeader from "./components/AdminHeader";
+import AdminHome from "./pages/admin/AdminHome";
+import ProductManagement from "./pages/admin/ProductManagement";
 
 function App() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
-  <>
-    <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/product' element={<Product />} />
-      <Route path="/register" element={<Register/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/test" element={<Test/>}/>
-    </Routes>
-    </BrowserRouter>
-  </>
-  )
+    <>
+      <BrowserRouter>
+        {currentUser && currentUser.isAdmin ? <AdminHeader /> : <Header />}
+        <Routes>
+          {currentUser && currentUser.isAdmin ? (
+            <>
+            <Route path="/admin" element={<AdminHome />} />
+            <Route path="/admin/product-management" element={<ProductManagement />} />
+            </>
+          ) : (
+            <>
+              <Route path="/" element={<Home />} />
+              <Route path="/product" element={<Product />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/test" element={<Test />} />
+            </>
+          )}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/admin/AdminHome.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function AdminHome() {
+  return (
+    <div>
+        <h2>AdminHome</h2>
+        <div className="">
+            So luong san pham:3
+        </div>
+        
+    </div>
+  )
+}
