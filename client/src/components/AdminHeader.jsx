93e952079bc9c67d9df7c310b933fffe2350401d
nampm9x <nampew2004@gmail.com>
2024-11-15 21:17:18 +0700

    logout for admin

--- client/src/components/AdminHeader.jsx
+++ client/src/components/AdminHeader.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
-
+import { Link, useNavigate } from 'react-router-dom';
+import { logout } from "../redux/user/userSlice";
+import { useDispatch } from "react-redux";
 
 export default function AdminHeader() {
+  const router=useNavigate()
+  const dispatch=useDispatch()
+
+  const handleLogout=async()=>{
+    try{
+    const res=await fetch("/api/auth/logout",{
+      method:"POST",
+      headers:{
+      "Content-Type":"application/json",
+      }
+    });
+    const data=await res.json();
+    if (res.ok){
+      router("/")
+      alert(data.message);
+      dispatch(logout())
+    } else{
+      alert(data.message);
+    } 
+  } catch(error){
+    console.log(error)
+  }
+} 
   return (
     <div className='flex gap-3 justify-center'>
         <Link className='border-2' to="/admin/users">Quan ly user</Link>
@@ -10,6 +34,7 @@ export default function AdminHeader() {
         <Link className='border-2' to="/admin/coupon">Quan ly ma giam gia</Link>
         <Link className='border-2' to="/admin/category-management">Quan ly danh muc</Link>
         <Link className='border-2' to="/admin/order">Quan ly don hang</Link>
+        <button type='button' onClick={handleLogout}>Logout</button>
     </div>
   )
 }
